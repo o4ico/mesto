e12cca2622e5bb65ac23fa7c3163813d2d8b849b
Darya Sokolova <o4ico@yandex.ru>
2023-02-14 06:31:07 +0300

    feat: сверстать сайт

---
+++ script.js
@@ -0,0 +1,50 @@
+let editButton = document.querySelector('.profile__edit-button');
+
+let popup = document.querySelector('.popup');
+
+let formElement = document.querySelector('.popup__form');
+
+let nameInput = document.querySelector('.popup__name');
+
+let jobInput = document.querySelector('.popup__about-me');
+
+let profileTitle = document.querySelector('.profile__title');
+
+let profileSubtitle = document.querySelector('.profile__subtitle');
+
+function openPopup () {
+
+    popup.classList.add('popup_opened');
+
+    nameInput.setAttribute('value', profileTitle.textContent);
+
+    jobInput.setAttribute('value', profileSubtitle.textContent);
+    
+};
+
+editButton.addEventListener('click', openPopup);
+
+let closeButton = document.querySelector('.popup__close-button');
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+};
+
+closeButton.addEventListener('click', closePopup);
+
+//value
+
+let submitButton = document.querySelector('.popup__submit-button');
+
+function handleFormSubmit (evt) {
+
+    evt.preventDefault();
+
+    profileTitle.textContent = nameInput.value;
+
+    profileSubtitle.textContent = jobInput.value;
+
+    closePopup();
+}
+
+submitButton.addEventListener('click', handleFormSubmit); 
