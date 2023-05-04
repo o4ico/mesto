2423c988d7e2c81ff701e5edba67ed3571d61cc4
Darya Sokolova <o4ico@yandex.ru>
2023-05-04 12:24:56 +0300

    refactor: рефакторинг классов

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,53 @@
-const path = require('path'); // подключаем path к конфигу вебпак
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключите плагин 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: {
+    main: './src/index.js'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-        publicPath: ''
+    publicPath: '',
   },
-  mode: 'development', // добавили режим разработчика
+  mode: 'development',
   devServer: {
-    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
-    compress: true, // это ускорит загрузку в режиме разработки
-    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-
-    open: true // сайт будет открываться сам при запуске npm run dev
+    static: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    port: 8080
   },
   module: {
-    rules: [ // rules — это массив правил
-      // добавим в него объект правил для бабеля
-      {
-        // регулярное выражение, которое ищет все js файлы
+    rules: [{
         test: /\.js$/,
-        // при обработке этих файлов нужно использовать babel-loader
         use: 'babel-loader',
-        // исключает папку node_modules, файлы в ней обрабатывать не нужно
         exclude: '/node_modules/'
       },
       {
-        // регулярное выражение, которое ищет все файлы с такими расширениями
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource'
+        type: 'asset/resource',
       },
       {
-        // применять это правило только к CSS-файлам
         test: /\.css$/,
-        // при обработке этих файлов нужно использовать
-        // MiniCssExtractPlugin.loader и css-loader
         use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader',
-          // добавьте объект options
-          options: { importLoaders: 1 }
-        },
-          // Добавьте postcss-loader
-        'postcss-loader']
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
       },
-      
-      ]
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html' // путь к файлу index.html
+      template: './src/index.html'
     }),
-    new CleanWebpackPlugin(), // использовали плагин
-    new MiniCssExtractPlugin() // подключение плагина для объединения файлов
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+
   ]
 }
